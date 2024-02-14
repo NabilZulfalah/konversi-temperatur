529aae3327f3eee2be3a83e41206ce71747d6c63
NabilZulfalah <nabilzulfalah@gmail.com>
2024-02-14 21:17:51 +0700

    first commit

---
+++ Nabil2.py
@@ -0,0 +1,11 @@
+#input untuk user
+celcius = int(input('Masukkan suhu dalam derajat Celcius :\n '))
+
+#bikin convert dri celcius ke fahrenheit
+fahrenheit = int((1.8 * celcius) + 32)
+
+#hitung
+dihitung = print(celcius,'derajat Celcius = derajat', fahrenheit, 'Fahrenheit')
+
+#print
+print(dihitung)
